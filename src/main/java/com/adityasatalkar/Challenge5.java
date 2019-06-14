08def5f038341664270e51f446a51f043e919473
aditya <adu.satalkar@gmail.com>
2019-06-14 11:43:04 -0500

    challenge completed

--- src/main/java/com/adityasatalkar/Challenge5.java
+++ src/main/java/com/adityasatalkar/Challenge5.java
@@ -1,14 +1,21 @@
 package com.adityasatalkar;
 
-/*
-
-{
-    "nextQuestion": "/fizzbot/questions/OKBu1t62WeGLscR0qqcftOx12jOLngMC00ooh9qCIbE",
-    "message": "Outstanding!",
-    "result": "correct"
-}
-
- */
-
 public class Challenge5 {
+
+    public static void main(String[] args) {
+        int[] numbers =
+            {558,
+            11,
+            702,
+            819,
+            422,
+            58,
+            670,
+            578,
+            126,
+            741};
+        System.out.println("{");
+        System.out.println("\t\"answer\": \"" + Challenge4.beepBoop(numbers) + "\"");
+        System.out.println("}");
+    }
 }
